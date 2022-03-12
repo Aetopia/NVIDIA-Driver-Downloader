0104d0af7c3c8d7c1f8eb2a60f98de8191b663b9
Aetopia <41850963+Aetopia@users.noreply.github.com>
2022-03-12 15:45:36 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-nuitka --follow-imports --onefile --standalone --remove-output --run nvddl.py
+set data=--windows-company-name=Aetopia --windows-product-name="NVIDIA Driver Downloader" --windows-file-version=1.0.0.0 --windows-product-version=1.0.0.0 --windows-file-description="NVIDIA Driver Downloader"
+nuitka --follow-imports --onefile --standalone --remove-output %data% --run nvddl.py
