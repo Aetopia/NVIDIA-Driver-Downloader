64c0c7afedf8ac04bc6ec030da7a479806ca8a19
Aetopia <41850963+Aetopia@users.noreply.github.com>
2022-03-12 15:46:43 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 set application_data=--windows-product-name="NVIDIA Driver Downloader" --windows-file-version=1.0.0.0 --windows-product-version=1.0.0.0 --windows-file-description="NVIDIA Driver Downloader"
-nuitka --follow-imports --onefile --standalone --remove-output %Application_data% --run nvddl.py
+nuitka --follow-imports --onefile --standalone --remove-output %application_data% --run nvddl.py
