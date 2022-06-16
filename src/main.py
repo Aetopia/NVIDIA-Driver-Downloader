15d12851532733bdd8c0702072342eaad984ff0a
Aetopia <kirbymario22@gmail.com>
2022-06-16 22:23:59 +0530

    Add KeyboardInterrupt Filter + Failsafe.

--- src/main.py
+++ src/main.py
@@ -2,163 +2,170 @@
 # GitHub Repository: https://github.com/Aetopia/NVIDIA-Driver-Downloader
 # Discord: https://dsc.gg/CTT
 # Under the MIT License: https://github.com/Aetopia/NVIDIA-Driver-Downloader/blob/main/LICENSE.md
+try:
+    from argparse import ArgumentParser, SUPPRESS, RawDescriptionHelpFormatter
+    from constants import HELP_DRIVER_OPTIONS, HELP_OPTIONS, HELP_ARGUMENTS, PROGRAM_DESCRIPTION
+    from functions import get_driver_versions, download, flags, extract, update
+    from textformat import fg, eol
+    from os import getcwd, path
+    from sys import argv, exit
+    import sys
+    from tempfile import gettempdir
 
-from argparse import ArgumentParser, SUPPRESS, RawDescriptionHelpFormatter
-from constants import HELP_DRIVER_OPTIONS, HELP_OPTIONS, HELP_ARGUMENTS, PROGRAM_DESCRIPTION
-from functions import get_driver_versions, download, flags, extract, update
-from textformat import fg, eol
-from os import getcwd, path
-from sys import argv, exit
-import sys
-from tempfile import gettempdir
+    def main():
+        parser = ArgumentParser(description=PROGRAM_DESCRIPTION,
+                                formatter_class=RawDescriptionHelpFormatter,
+                                add_help=False, usage=SUPPRESS)
 
+        parser.add_argument('--help', action='help', help=SUPPRESS)
 
-def main():
-    parser = ArgumentParser(description=PROGRAM_DESCRIPTION,
-                            formatter_class=RawDescriptionHelpFormatter,
-                            add_help=False, usage=SUPPRESS)
+        arguments = parser.add_argument_group(title=' Arguments',
+                                              description=HELP_ARGUMENTS)\
+            .add_mutually_exclusive_group()
 
-    parser.add_argument('--help', action='help', help=SUPPRESS)
+        driver_options = parser.add_argument_group(title=' Driver Options',
+                                                   description=HELP_DRIVER_OPTIONS)
 
-    arguments = parser.add_argument_group(title=' Arguments',
-                                          description=HELP_ARGUMENTS)\
-        .add_mutually_exclusive_group()
+        options = parser.add_argument_group(title=' Options',
+                                            description=HELP_OPTIONS)
 
-    driver_options = parser.add_argument_group(title=' Driver Options',
-                                               description=HELP_DRIVER_OPTIONS)
+        arguments.add_argument('--list', '-ls',
+                               action='store_true',
+                               help=SUPPRESS)
 
-    options = parser.add_argument_group(title=' Options',
-                                        description=HELP_OPTIONS)
+        arguments.add_argument('--download', '-dl',
+                               nargs='?',
+                               help=SUPPRESS,
+                               metavar='Driver Version')
 
-    arguments.add_argument('--list', '-ls',
-                           action='store_true',
-                           help=SUPPRESS)
+        arguments.add_argument('--extract', '-e',
+                               nargs=1,
+                               help=SUPPRESS,
+                               metavar='<Driver File>')
 
-    arguments.add_argument('--download', '-dl',
-                           nargs='?',
-                           help=SUPPRESS,
-                           metavar='Driver Version')
+        arguments.add_argument('--update', '-u',
+                               action='store_true',
+                               help=SUPPRESS)
 
-    arguments.add_argument('--extract', '-e',
-                           nargs=1,
-                           help=SUPPRESS,
-                           metavar='<Driver File>')
+        driver_options.add_argument('--studio', '-stu',
+                                    action='store_true',
+                                    help=SUPPRESS)
 
-    arguments.add_argument('--update', '-u',
-                           action='store_true',
-                           help=SUPPRESS)
+        driver_options.add_argument('--standard', '-std',
+                                    action='store_true',
+                                    help=SUPPRESS)
 
-    driver_options.add_argument('--studio', '-stu',
-                                action='store_true',
-                                help=SUPPRESS)
+        driver_options.add_argument('--full', '-f',
+                                    action='store_true',
+                                    help=SUPPRESS)
 
-    driver_options.add_argument('--standard', '-std',
-                                action='store_true',
-                                help=SUPPRESS)
+        driver_options.add_argument('--setup', '-s',
+                                    action='store_true',
+                                    help=SUPPRESS)
 
-    driver_options.add_argument('--full', '-f',
-                                action='store_true',
-                                help=SUPPRESS)
+        options.add_argument('--components', '-c',
+                             action='store',
+                             metavar=('Component', 'Components'),
+                             default=[],
+                             nargs='+',
+                             help=SUPPRESS)
 
-    driver_options.add_argument('--setup', '-s',
-                                action='store_true',
-                                help=SUPPRESS)
+        options.add_argument('--output', '-o',
+                             nargs='?',
+                             default=gettempdir(),
+                             action='store',
+                             help=SUPPRESS,
+                             metavar='Directory')
 
-    options.add_argument('--components', '-c',
-                         action='store',
-                         metavar=('Component', 'Components'),
-                         default=[],
-                         nargs='+',
-                         help=SUPPRESS)
+        options.add_argument('--flags',
+                             action='store',
+                             metavar=('Flag', 'Flags'),
+                             nargs='+',
+                             default=[],
+                             help=SUPPRESS)
 
-    options.add_argument('--output', '-o',
-                         nargs='?',
-                         default=gettempdir(),
-                         action='store',
-                         help=SUPPRESS,
-                         metavar='Directory')
+        parser.add_argument('--no-stdout',
+                            action='store_true',
+                            help=SUPPRESS)
 
-    options.add_argument('--flags',
-                         action='store',
-                         metavar=('Flag', 'Flags'),
-                         nargs='+',
-                         default=[],
-                         help=SUPPRESS)
+        args = parser.parse_args()
+        flags(args.flags)
+        if args.no_stdout:
+            sys.stdout = None
 
-    parser.add_argument('--no-stdout',
-                        action='store_true',
-                        help=SUPPRESS)
+        if len(argv) != 1:
+            if not ('-dl' in argv or '--download' in argv
+                    or '-u' in argv or '--update' in argv
+                    or '--extract' in argv or '-e' in argv
+                    or '-ls' in argv or '--list' in argv):
+                parser.print_usage()
+                print(f'{fg.lred}Error: Options must be used with arguments.{eol}')
+                exit()
 
-    args = parser.parse_args()
-    flags(args.flags)
-    if args.no_stdout:
-        sys.stdout = None
+            if args.output is None:
+                args.output = getcwd()
+            match args.studio:
+                case True: driver_type = 'Studio'
+                case False: driver_type = 'Game Ready'
 
-    if len(argv) != 1:
-        if not ('-dl' in argv or '--download' in argv
-                or '-u' in argv or '--update' in argv
-                or '--extract' in argv or '-e' in argv
-                or '-ls' in argv or '--list' in argv):
-            parser.print_usage()
-            print(f'{fg.lred}Error: Options must be used with arguments.{eol}')
-            exit()
+            match args.standard:
+                case True:
+                    type = 'std'
+                    driver_type = f'Standard {driver_type}'
+                case False:
+                    type = 'dch'
+                    driver_type = f'DCH {driver_type}'
 
-        if args.output is None:
-            args.output = getcwd()
-        match args.studio:
-            case True: driver_type = 'Studio'
-            case False: driver_type = 'Game Ready'
+            if args.list is True:
+                print(f'{fg.lyellow}{driver_type} Drivers:{eol}')
+                driver_versions = get_driver_versions(
+                    studio_drivers=args.studio, type=type)
 
-        match args.standard:
-            case True:
-                type = 'std'
-                driver_type = f'Standard {driver_type}'
-            case False:
-                type = 'dch'
-                driver_type = f'DCH {driver_type}'
+                for index, driver_version in enumerate(driver_versions):
+                    if index == 0:
+                        print(f' {fg.lgreen}~ {driver_version}{eol}')
+                    elif index+1 == len(driver_versions):
+                        print(f' {fg.lviolet}# {driver_version}{eol}')
+                    else:
+                        print(f' {fg.lbeige}> {driver_version}{eol}')
 
-        if args.list is True:
-            print(f'{fg.lyellow}{driver_type} Drivers:{eol}')
-            driver_versions = get_driver_versions(
-                studio_drivers=args.studio, type=type)
+            elif args.extract is not None:
+                print(
+                    f'{fg.lyellow}Extracting ({path.split(args.extract[0])[1].strip()})...{eol}')
+                extract(args.extract[0], output=args.output,
+                        components=args.components,
+                        full=args.full, setup=args.setup)
 
-            for index, driver_version in enumerate(driver_versions):
-                if index == 0:
-                    print(f' {fg.lgreen}~ {driver_version}{eol}')
-                elif index+1 == len(driver_versions):
-                    print(f' {fg.lviolet}# {driver_version}{eol}')
-                else:
-                    print(f' {fg.lbeige}> {driver_version}{eol}')
+            elif args.update is True:
+                update(studio_drivers=args.studio,
+                       components=args.components, setup=args.setup)
 
-        elif args.extract is not None:
-            print(
-                f'{fg.lyellow}Extracting ({path.split(args.extract[0])[1].strip()})...{eol}')
-            extract(args.extract[0], output=args.output,
-                    components=args.components,
-                    full=args.full, setup=args.setup)
+            elif args.download is not None:
+                print(f'{fg.lyellow}Downloading {driver_type} Driver...{eol}')
+                driver_version = args.download
+                print(f'{fg.lyellow}Version: {driver_version}{eol}')
 
-        elif args.update is True:
-            update(studio_drivers=args.studio,
-                   components=args.components, setup=args.setup)
+                download(driver_version=driver_version, studio_drivers=args.studio,
+                         type=type, output=args.output,
+                         full=args.full, components=args.components, setup=args.setup)
 
-        elif args.download is not None:
-            print(f'{fg.lyellow}Downloading {driver_type} Driver...{eol}')
-            driver_version = args.download
-            print(f'{fg.lyellow}Version: {driver_version}{eol}')
+            elif args.download is None:
+                print(
+                    f'{fg.lyellow}Downloading the Latest {driver_type} Driver...{eol}')
 
-            download(driver_version=driver_version, studio_drivers=args.studio,
-                     type=type, output=args.output,
-                     full=args.full, components=args.components, setup=args.setup)
+                download(studio_drivers=args.studio, type=type,
+                         output=args.output, full=args.full,
+                         components=args.components, setup=args.setup)
+        else:
+            parser.print_help()
 
-        elif args.download is None:
-            print(f'{fg.lyellow}Downloading the Latest {driver_type} Driver...{eol}')
-
-            download(studio_drivers=args.studio, type=type,
-                     output=args.output, full=args.full,
-                     components=args.components, setup=args.setup)
-    else:
-        parser.print_help()
-
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        try:
+            main()
+            exit(0)
+        except KeyboardInterrupt:
+            print(f'\n{fg.lred}Warning: Operation Cancelled.{eol}')
+            exit(1)
+# Failsafe in case, NVDDL initialization was not successful.
+except KeyboardInterrupt:
+    pass
